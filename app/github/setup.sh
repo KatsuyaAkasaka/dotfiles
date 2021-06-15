e3b41b28f61ee2d12cf19e2e61cfdc92d01a3dab
KatsuyaAkasaka <akasakatora1208@gmail.om>
2021-06-15 21:42:16 +0900

    fix

--- app/github/setup.sh
+++ app/github/setup.sh
@@ -1,8 +1,10 @@
 #! /bin/zsh
 
-ssh-keygen -t rsa -f ~/.ssh/id_rsa_github
+cd ~/.ssh
+ssh-keygen -t rsa
+cd ~/dotfiles
 
-ln -s ./ssh_config ~/.ssh/config
+ln -s ~/dotfiles/app/github/ssh_config ~/.ssh/config
 
-git config --global user.name "Katsuya Akasaka"
+git config --global user.name "KatsuyaAkasaka"
 git config --global user.email "akasakatora1208@gmail.om"
