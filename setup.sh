8d482bf80407741762755331c034474202ebe454
KatsuyaAkasaka <akasakatora1208@gmail.com>
2021-09-30 22:56:44 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -3,59 +3,27 @@
 # 必ずホームディレクトリ上で実行すること!!
 cd ~
 
-# system.logを初期化
-: > ./system.log
-
 # 実行内容一覧
 HOMEBREW_INSTALL="Install homebrew"
 GIT_INSTALL="Install git"
 GIT_CLONE="Clone dotfiles"
 
-EXECUTE_LIST=("$HOMEBREW_INSTALL", "$GIT_INSTALL", "$GIT_CLONE")
-
-# loadingの標準出力用関数
-i=1
-loading () {
-	echo "[Step$i/${#EXECUTE_LIST[@]}]"
-    (( i++ ))
-	while :; do
-		for X in '-' '\' '|' '/'; do
-			printf "[$X]$2\r";
-			sleep 0.1;
-		done;
-        if test ! $(ps -p $1 > /dev/null); then
-            break
-        fi
-	done
-	echo "\nfinished!"
-}
-
 # homebrewのインストール
-echo "[$HOMEBREW_INSTALL]" >> ./system.log
+echo "Install homebrew"
 if test ! $(which brew); then
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" >> ./system.log
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
-	echo "[INFO] already intalled homebrew" >> ./system.log
-fi &
-homebrewPid=$!
-
-loading $homebrewPid "Install homebrew"
+	echo "[INFO] already intalled homebrew"
+fi
 
 # gitのインストール
-echo "[$GIT_INSTALL]" >> ./system.log
+echo "Install git"
 if test ! $(which git); then
-	brew install git >> ./system.log
+	brew install git
 else
-	echo "[INFO] already intalled git" >> ./system.log
-fi &
-gitPid=$!
-
-loading $gitPid "Install git"
+	echo "[INFO] already intalled git"
+fi
 
 # dotfilesのclone
-echo "[$GIT_CLONE]" >> ./system.log
-git clone https://github.com/KatsuyaAkasaka/dotfiles >> ./system.log &
-clonePid=$!
-
-loading $clonePid "Clone dotfiles"
-wait $homebrewPid $gitPid $clonePid
+echo "Clone dotfiles"
+git clone https://github.com/KatsuyaAkasaka/dotfiles
--- zsh/set.sh
+++ zsh/set.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-echo "installing homebrew..."
-# Check for Homebrew and install if we don't have it
-if test ! $(which brew); then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-
 # Update Homebrew recipes
 brew update
 
