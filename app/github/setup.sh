ee983124a0b1410849946a27ce23fcb4b96cc751
KatsuyaAkasaka <akasakatora1208@gmail.com>
2023-06-03 17:32:18 +0900

    update github

--- app/github/setup.sh
+++ app/github/setup.sh
@@ -2,7 +2,7 @@
 
 mkdir $HOME/.ssh
 
-ssh-keygen -t rsa -f $HOME/.ssh/id_rsa_github
+ssh-keygen -t ed25519 -C "akasakatora1208@gmail.com" -f $HOME/.ssh/id_ed25519_github
 cd $HOME/dotfiles
 
 ln -s $HOME/dotfiles/app/github/ssh_config $HOME/.ssh/config
