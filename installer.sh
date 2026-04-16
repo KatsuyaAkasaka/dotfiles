21b894b86b55560554590daba9c1d4f692e5e044
Katsuya Akasaka <akasakatora1208@gmail.com>
2026-04-17 08:34:57 +0900

    rm

--- installer.sh
+++ installer.sh
@@ -12,9 +12,4 @@ echo "vscode vim enable to repeating..."
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false         # For VS Code
 defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false # For VS Code Insider
 
-echo "gcloud setup..."
-gcloud config configurations create sakas
-gcloud config set project rich-suprstate-283005
-gcloud auth login
-
 echo "Finished!! Let's go!!"
